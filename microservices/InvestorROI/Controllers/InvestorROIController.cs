67a9c5d9ee4cf62512d0b1a1b68e96043dbb1767
sormitaibm <sormitach@in.ibm.com>
2021-07-05 18:08:22 +0530

    InvestorROI db connection working

---
+++ microservices/InvestorROI/Controllers/InvestorROIController.cs
@@ -0,0 +1,33 @@
+﻿using InvestorROI.Model;
+using InvestorROI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestorROI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvestorROIController : ControllerBase
+    {
+        readonly IInvestorROIRepository<Actor> repos;
+        private readonly ILogger<InvestorROIController> logger;
+        public InvestorROIController(IInvestorROIRepository<Actor> _repos, ILogger<InvestorROIController> _logger)
+        {
+            repos = _repos;
+            logger = _logger;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Actor> Get()
+        {
+            var actors = repos.GetAll();
+            return actors.ToList();
+        }
+    }
+}
